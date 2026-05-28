b65e4e0bbf162e7425d0823d3d1917991726c158
Kathy Sagredo <136348407+kathySagredo@users.noreply.github.com>
2026-05-28 11:28:23 -0400

    Use environment variable for server port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Permitir recibir JSON
 app.use(express.json());
